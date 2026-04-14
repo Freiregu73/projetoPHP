475a894988081adc68713d64984b829b05a1379c
Freiregu73 <gustavofreire191@gmail.com>
2026-04-14 20:17:55 -0300

    vetor

---
+++ array_linha.php
@@ -0,0 +1,29 @@
+<?php 
+
+	$idade = array();
+
+	$idade[0] = 10;
+	$idade[8] = 5;
+
+	$valor = array(12,20,30,40);
+
+	$nomes = array("Maria", "José", "Pedro", "Paulo");
+
+	$salario[] = 1500.52;
+	$salario[] = 1250.35;
+	$salario[] = 1567.62;
+	$salario[] = 1590.92;
+
+	echo($valor[0] . "<br>");
+	echo($nomes[3] . "<br>");
+	echo($salario[2] . "<br>");
+
+	var_dump($valor);
+	echo "<br>";
+	var_dump($nomes);
+	echo "<br>";
+	var_dump($idade);
+	echo "<br>";
+	var_dump($salario);
+ ?>
+ ?>
