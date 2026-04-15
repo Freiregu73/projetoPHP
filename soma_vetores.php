062d408e7b038d0faf67d517c66391ee6345c092
Freiregu73 <gustavofreire191@gmail.com>
2026-04-14 21:28:20 -0300

    vetor

--- soma_vetores.php
+++ soma_vetores.php
@@ -4,9 +4,16 @@ $num1 = array(10,20,30,40);
 
 $num2 = array(50,60,70,80);
 
-var_dump($num1);
-echo "<br>"
-var_dump($num2);
+$resultado = array();
+
+	for ($i=0; $i < count($num1); $i++) { 
+		$resultado[$i] = $num1[$i] + $num2[$i];
+	}
+
+	for ($i=0; $i < count($num1); $i++) { 
+		echo "$resultado[$i] <br>";
+	}
+
 
 
 
