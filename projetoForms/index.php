b8dd07b3d59b47d80e56e370ace84811594841a3
Freiregu73 <gustavofreire191@gmail.com>
2026-05-05 19:22:54 -0300

    organizaçao

---
+++ projetoForms/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Formularios PHP</title>
+	<link rel="stylesheet" type="css/style.css" href="">
+</head>
+<body>
+
+	<h1>Formulários PHP</h1>
+
+	<form action="#" method="post">
+		<label for="email">E-mail:</label>
+		<input type="email" name="email" id="email">
+		<label for="senha">Senha:</label>
+		<input type="password" name="senha" id="senha">
+		<input type="submit" name="entrar" value="Entrar">
+		<input type="reset" name="limpar" value="Limpar">
+		
+	</form>
+
+
+	<script src="js/script.js"></script>
+</body>
+</html>
